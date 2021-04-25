26b6ae5ac88ed788bd736c8644ddfe4f28874bf3
shohanur021@gmail.com <shohanur021@gmail.com>
2021-04-26 01:29:31 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Chart from './Components/Chart/Chart';
+import Second from './Components/2ndChart/Second';
+
+function App() {
+  return (
+    <div>
+       <Chart></Chart>
+       <br></br><br></br><br></br>
+       <Second></Second>
+    </div>
+  );
+}
+
+export default App;
